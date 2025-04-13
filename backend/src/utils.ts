7ca0a2e13394f8619ee17f2cc5896173db0af555
Tom Kwok <junhaotom@gmail.com>
2025-04-13 15:06:22 +1000

    Add class Player, Game and their tests

---
+++ backend/src/utils.ts
@@ -0,0 +1,28 @@
+export enum GameMode {
+  OneLine = 'One Line',
+  LineLengthLimit = 'Line Length Limit',
+}
+
+export function getNextElement<T>(array: Array<T>, element: T) {
+  const currentIndex = array.indexOf(element)
+  return currentIndex === -1 ? null : array[(currentIndex + 1) % array.length]
+}
+
+export const SOCKET_EVENTS_OUTBOUND = {
+  DRAW: 'draw',
+  CONNECT: 'connect',
+  INITIAL_DATA: 'initial-data',
+  UPDATE_TURN: 'update-turn',
+  CLEAR_CANVAS: 'clear-canvas',
+  SELECTED_GAME_MODE: 'selected-game-mode',
+} as const
+
+export const SOCKET_EVENTS_INBOUND = {
+  CONNECTION: 'connection',
+  DISCONNECT: 'disconnect',
+
+  DRAW: 'draw',
+  END_TURN: 'end-turn',
+  CLEAR_CANVAS: 'clear-canvas',
+  SELECT_GAME_MODE: 'select-game-mode',
+} as const
